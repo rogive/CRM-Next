e68ccae4a705fe4f5a5f63aad18ed256978cb85f
rogive <ierodriguezgo@gmail.com>
2021-02-14 23:01:59 -0500

    base-CRM

---
+++ pages/orders.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+export default function Orders() {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light"> Pedidos </h1>
+      </Layout>
+    </div>
+  );
+}
